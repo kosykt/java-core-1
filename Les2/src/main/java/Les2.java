2cdaa0fe76148fca0885e108f6568174355956d0
kosykt <kosvasl09@gmail.com>
2021-02-23 02:19:12 +0900

    les 2

--- Les2/src/main/java/Les2.java
+++ Les2/src/main/java/Les2.java
@@ -87,16 +87,6 @@ public class Les2 {
         //HW 2.6
 
         int[] arr6 = {2, 2, 2, 6};
-        int[] check1 = new int[arr6.length];
-        int[] check2 = new int[arr6.length];
-        int number = arr6.length;
-
-        for (int i = 0; i < arr6.length; i++){
-            while (true){
-                check1[i] = arr6[i];
-
-            }
-        }
 
     }
 
