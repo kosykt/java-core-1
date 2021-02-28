740cf41bd6aa67f2aaa416daad52459444e6f8c9
kosykt <kosvasl09@gmail.com>
2021-02-28 21:43:25 +0900

    HW-3

--- Les3/src/main/java/Les3.java
+++ Les3/src/main/java/Les3.java
@@ -33,18 +33,19 @@ public class Les3 {
                     game(ans);
                 }
                 break;
-            }else {
-                System.out.println("Вы проиграли!");
-                System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет");
-                Scanner play = new Scanner(System.in);
-                int ans = play.nextInt();
-                if (ans == 1){
-                    game(ans);
-                }
-                break;
             }
             System.out.println("Введите другое число");
             pnum = playernum.nextInt();
         }while (trycount != 3);
+
+        if (trycount == 3){
+            System.out.println("Вы проиграли!");
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет");
+            Scanner play = new Scanner(System.in);
+            int ans = play.nextInt();
+            if (ans == 1){
+                game(ans);
+            }
+        }
     }
 }
