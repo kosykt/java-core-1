e4bbec63095d29b02fe8fd02ac082c9a859e92c8
kosykt <kosvasl09@gmail.com>
2021-03-05 00:44:02 +0900

    HW4workers

--- les5/src/main/java/HW5.java
+++ les5/src/main/java/HW5.java
@@ -1,15 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class HW5 {
 
     public static void main(String[] args) {
-        Worker[] workersArray = new Worker[5];
+        Worker[] workersArray = new Worker[6];
         workersArray[0] = new Worker("Ivanov Ivan", "Engineer", "ivan@mailbox.com", 8999000, 30000, 30);
         workersArray[1] = new Worker("Petov Roman", "Engineer", "roman@mailbox.com", 8999111, 40000, 20);
         workersArray[2] = new Worker("Sidorov Ilia", "Engineer", "ilia@mailbox.com", 8999222, 50000, 50);
         workersArray[3] = new Worker("Vasilev Konstantin", "Engineer", "konstantin@mailbox.com", 8999333, 50000, 60);
         workersArray[4] = new Worker("Borisov Petr", "Engineer", "petr@mailbox.com", 8999444, 60000, 25);
-        workersArray[5] = new Worker("Novikov Ivan", "Engineer", "ivan@mailbox.com", 8999555, 70000, 35);
-
-        
-
+        workersArray[5] = workersArray[4];
     }
 }
