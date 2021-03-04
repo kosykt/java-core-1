832d8c0d2f1222c5f49738a3b677a09dca8c57e0
kosykt <kosvasl09@gmail.com>
2021-03-04 23:46:32 +0900

    HW5

--- les5/src/main/java/HW5.java
+++ les5/src/main/java/HW5.java
@@ -1,6 +1,7 @@
 public class HW5 {
 
     public static void main(String[] args) {
-        
+        Worker w1 = new Worker("Ivanov Ivan", "Engineer", "ivan@mailbox.com", 892312312, 30000, 30);
+
     }
 }
---
+++ les5/src/main/java/Worker.java
@@ -0,0 +1,17 @@
+public class Worker {
+    private String name;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+
+    public Worker(String name, String position, String email, int phoneNumber, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+}
