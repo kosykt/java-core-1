812c411df67c86d0c3b80f2cc1d781a0e29271e5
kosykt <kosvasl09@gmail.com>
2021-03-04 14:56:08 +0900

    HW$

---
+++ les4/src/main/java/qwe.java
@@ -0,0 +1,13 @@
+public class qwe {
+    public static void main(String[] args) {
+        String str = "hello";
+//        charAt
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(4));
+//        substring
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(2,4));
+        System.out.println(str.substring(4));
+    }
+}
