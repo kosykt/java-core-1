45f4d89c167155be54ef38c0fe325267bdd89f4c
kosykt <kosvasl09@gmail.com>
2021-03-10 19:33:29 +0900

    HW6

---
+++ les6/src/main/java/Les6/HW6.java
@@ -0,0 +1,14 @@
+package Les6;
+
+public class HW6 {
+
+    public static void main(String[] args) {
+
+        Cat c1 = new Cat("Barsik", "White", 4, 900, 90);
+        Dog d1 = new Dog("Bobik", "Black", 8, 500, 10);
+        System.out.println(c1.run(50));
+        System.out.println(c1.swim(1));
+        System.out.println(d1.run(550));
+        System.out.println(d1.swim(51));
+    }
+}
