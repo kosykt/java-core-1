ba028e094f42d4ff02c9922a684ca76e74ce5521
kosykt <kosvasl09@gmail.com>
2021-03-13 19:02:30 +0900

    add_fullness

--- les7/src/main/java/HW7/Cat.java
+++ les7/src/main/java/HW7/Cat.java
@@ -3,11 +3,13 @@ package HW7;
 public class Cat {
     private String name;
     private int appetite;
+    private boolean fullness;
 
 
-    public Cat(String name, int appetite) {
+    public Cat(String name, int appetite, boolean fullness) {
         this.name = name;
         this.appetite = appetite;
+        this.fullness = fullness;
     }
 
     public void eat(Plate p) {
--- les7/src/main/java/HW7/HW7.java
+++ les7/src/main/java/HW7/HW7.java
@@ -2,8 +2,8 @@ package HW7;
 
 public class HW7 {
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 5);
-        Plate plate = new Plate(1);
+        Cat cat = new Cat("Barsik", 5, false);
+        Plate plate = new Plate(5);
 
 
         plate.info();
