bc0153b670e5b73127eca0ce91aad1f44494dd7f
kosykt <kosvasl09@gmail.com>
2021-03-14 18:59:02 +0900

    bug

--- les8/src/main/java/WH8/HW8.java
+++ les8/src/main/java/WH8/HW8.java
@@ -8,25 +8,26 @@ public class HW8 {
     static Robot[] robotsArray = new Robot[1];
 
     public static void main(String[] args) {
-        humansArray[0] = new Human("Henry", 200,20);
+        humansArray[0] = new Human("Henry", 100,20);
         catsArray[0] = new Cat("Barsik", 100,10);
         robotsArray[0] = new Robot("R2D2", 300,30);
 
         startRun();
-        startJump();
+//        startJump();
+
+    }
+
+    private static void startRun() {
+        for (int i = 0; i < humansArray.length; i++) {
+            System.out.println(t.canRun(humansArray[i].getRun()));
+
+        }
 
     }
 
     private static void startJump() {
         for (int i = 0; i < humansArray.length; i++) {
-            System.out.println(t.canRun(humansArray[i].getRun()));
-        }
-    }
-
-    private static void startRun() {
-        for (int i = 0; i < humansArray.length; i++) {
             System.out.println(w.canJump(humansArray[i].getJump()));
         }
-
     }
 }
