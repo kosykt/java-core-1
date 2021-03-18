b081e66dee5580e1a3693519e37bd853b6735a28
kosykt <kosvasl09@gmail.com>
2021-03-18 20:50:43 +0900

    HW8_done

--- les8/src/main/java/WH8/HW8.java
+++ les8/src/main/java/WH8/HW8.java
@@ -5,13 +5,17 @@ public class HW8 {
     static ObstacleСourse[] obstacleСourseArray = new ObstacleСourse[2];
 
     public static void main(String[] args) {
-        competitorsArray[0] = new Human("Henry", 200, 10);
+        competitorsArray[0] = new Human("Harry", 200, 10);
         competitorsArray[1] = new Cat("Barsik", 100, 10);
         competitorsArray[2] = new Robot("R2D2", 300,30);
 
         obstacleСourseArray[0] = new Treadmill(150);
         obstacleСourseArray[1] = new Wall(15);
 
+        start();
+    }
+
+    private static void start() {
         for (int i = 0; i < competitorsArray.length; i++) {
             if (obstacleСourseArray[0].trying(competitorsArray[i].getRun(), competitorsArray[i].getName())){
                 if (obstacleСourseArray[1].trying(competitorsArray[i].getJump(), competitorsArray[i].getName())){
@@ -19,6 +23,5 @@ public class HW8 {
                 }
             }
         }
-
     }
 }
