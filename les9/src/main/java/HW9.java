248b957ac1d2a44255e3a6b5f77b9e091ff16e1e
kosykt <kosvasl09@gmail.com>
2021-03-21 16:42:31 +0900

    cleaning code

--- les9/src/main/java/HW9.java
+++ les9/src/main/java/HW9.java
@@ -2,7 +2,8 @@ public class HW9 {
 
     public static void main(String[] args){
         String[][] arr = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"},
-                          {"9", "10", "11", "12", "1"}, {"13", "14", "15", "16l"}};
+                          {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+        
         try {
             try {
                 int summ = checkMyArray(arr);
@@ -17,33 +18,36 @@ public class HW9 {
 
     private static int checkMyArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
         if (arr.length != 4){
-            System.out.println("не верное количество внутренних одномерных массивов\n");
+            System.out.println("не верное количество внутренних одномерных массивов");
             throw new MyArraySizeException();
         }
+
         for (int i = 0; i < 4; i++) {
             if (arr[i].length != 4){
-                System.out.printf("не верное колличество ячеек в %d-м внутреннем массиве\n", i);
+                System.out.printf("не верное колличество ячеек в %d-м внутреннем массиве", i);
                 throw new MyArraySizeException();
             }
         }
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 try {
                     Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
-                    System.out.printf("не верное наполнение ячееки [%d][%d]\n", i, j);
+                    System.out.printf("не верное наполнение ячееки [%d][%d]", i, j);
                     arr[i][j] = String.valueOf(0);
                     throw new MyArrayDataException();
                 }
-
             }
         }
+
         int summ = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 summ += Integer.parseInt(arr[i][j]);
             }
         }
+
         return summ;
     }
 }
