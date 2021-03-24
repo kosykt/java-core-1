a015a952865739c1d3420b195e810f1c4a468ce5
kosykt <kosvasl09@gmail.com>
2021-03-24 21:47:33 +0900

    for_if

--- les10/src/main/java/HW10.java
+++ les10/src/main/java/HW10.java
@@ -8,11 +8,23 @@ public class HW10 {
         myString.add("aaa");myString.add("bbb");myString.add("ccc");myString.add("ddd");myString.add("eee");
         myString.add("fff");myString.add("ggg");myString.add("hhh");myString.add("iii");myString.add("jjj");
 
-        myString.add("WWW");myString.add("WWW");myString.add("WWW");
+        myString.add("WWW");myString.add("WWW");myString.add("WWW");myString.add("WWW");myString.add("WWW");myString.add("WWW");
 
         myString.add("XXX");myString.add("XXX");myString.add("XXX");
 
-        
+
+        try {
+            for (int i = 0; i < myString.size(); i++) {
+                String strP = myString.get(i);
+                String strN = myString.get(i + 1);
+                if (strN == strP){
+                    myString.remove(i);
+                    i--;
+                }
+            }
+        }catch (Exception e){
+
+        }
 
         System.out.println(myString);
     }
