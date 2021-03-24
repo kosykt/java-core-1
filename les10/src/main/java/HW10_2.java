307c8b648508a35afe3c2424efe6bc455fcd50cd
kosykt <kosvasl09@gmail.com>
2021-03-25 00:38:47 +0900

    PhoneBook

--- les10/src/main/java/HW10_2.java
+++ les10/src/main/java/HW10_2.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HW10_2 {
 
     public static void main(String[] args) {
-
+        List<PhoneBook> phoneBookList = new ArrayList<>(Arrays.asList(
+                new PhoneBook("AAA", 111),
+                new PhoneBook("BBB", 222),
+                new PhoneBook("CCC", 333)));
     }
 }
---
+++ les10/src/main/java/PhoneBook.java
@@ -0,0 +1,10 @@
+public class PhoneBook {
+
+    private String name;
+    private int number;
+
+    public PhoneBook(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+}
