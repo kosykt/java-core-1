6f053bc03c64f4a2edda45dd2b381800fd8094ef
kosykt <kosvasl09@gmail.com>
2021-03-24 22:46:25 +0900

    unique

--- les10/src/main/java/HW10.java
+++ les10/src/main/java/HW10.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HW10 {
 
@@ -7,12 +8,13 @@ public class HW10 {
 
         myString.add("aaa");myString.add("bbb");myString.add("ccc");myString.add("ddd");myString.add("eee");
         myString.add("fff");myString.add("ggg");myString.add("hhh");myString.add("iii");myString.add("jjj");
-
         myString.add("WWW");myString.add("WWW");myString.add("WWW");myString.add("WWW");myString.add("WWW");myString.add("WWW");
-
         myString.add("XXX");myString.add("XXX");myString.add("XXX");
 
+        unique(myString);
+    }
 
+    private static void unique(List<String> myString) {
         try {
             for (int i = 0; i < myString.size(); i++) {
                 String strP = myString.get(i);
@@ -22,10 +24,7 @@ public class HW10 {
                     i--;
                 }
             }
-        }catch (Exception e){
-
-        }
-
+        }catch (Exception e){ }
         System.out.println(myString);
     }
 }
