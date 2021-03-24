cc3f42c60c6409ee54ff9d3e7cfd2cad3043fd67
kosykt <kosvasl09@gmail.com>
2021-03-24 21:35:16 +0900

    clean

--- les10/src/main/java/HW10.java
+++ les10/src/main/java/HW10.java
@@ -12,32 +12,7 @@ public class HW10 {
 
         myString.add("XXX");myString.add("XXX");myString.add("XXX");
 
-
-
-        Iterator<String> iter = myString.iterator();
-        ListIterator<String> listIter = myString.listIterator();
-        //вариант 1
-        while (iter.hasNext()){
-            System.out.println(1);
-        }
-        //вариант 2
-        while (listIter.hasPrevious()){
-            System.out.println(2);
-        }
-        //вариант 3
-        while (iter.hasNext()){
-            String str = iter.next();
-            if (str.equals("aaa")) {
-                iter.remove();
-            }
-        }
-        //вариант 4
-        while (listIter.hasNext()){
-            String str = listIter.previous();
-            if (str.equals("bbb")){
-                iter.remove();
-            }
-        }
+        
 
         System.out.println(myString);
     }
