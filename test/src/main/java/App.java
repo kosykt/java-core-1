2842068016319bf00352037fb809d3ec5499a40f
kosykt <kosvasl09@gmail.com>
2021-03-26 22:02:52 +0900

    test/hw5

--- test/src/main/java/App.java
+++ test/src/main/java/App.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        list.add("eee");
+        list.add("fff");
+        list.add("ggg");
+        list.add("hhh");
+        list.add("iii");
+        list.add("jjj");
+        list.add("aaa");
+
+//        list.stream().sorted().filter(n  -> n.equals(n)).forEach(System.out::println);
+
+        Set<String> set = new HashSet<>(list);
+
+//        set.stream().forEach(System.out::println);
+
+        list.stream().sorted()
+                .filter(n -> "aaa".equals(n))
+                .forEach(System.out::println);
+
+    }
 }
