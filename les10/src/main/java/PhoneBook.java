2cd6e4b317659ef9d58ea5d025dc4a72c182da98
kosykt <kosvasl09@gmail.com>
2021-03-27 15:43:52 +0900

    values.toString

--- les10/src/main/java/PhoneBook.java
+++ les10/src/main/java/PhoneBook.java
@@ -4,28 +4,29 @@ import java.util.List;
 import java.util.Map;
 
 public class PhoneBook {
-
-    class NumList{
-        private List<Integer> numList;
-
-        public NumList(){
-            numList = new ArrayList<>();
-        }
-
-        public void addNum(Integer num){
-            numList.add(num);
-        }
-
-        public int getNum(){
-            return numList.get(1);
-        }
-    }
+//    что-то не то
+//    class NumList{
+//        private List<Integer> numList;
+//
+//        public NumList(){
+//            numList = new ArrayList<>();
+//        }
+//
+//        public void addNum(Integer num){
+//            numList.add(num);
+//        }
+//
+//        public int getNum(){
+//            return numList.get(1);
+//        }
+//    }
 
     private Map<String, Integer> phonebook;
 
     public PhoneBook(){
         phonebook = new HashMap<>();
     }
+//                                  List?Map?
 //              add(String name, список телефонов)
     public void add(String name, Integer number){
         phonebook.put(name, number);
@@ -34,8 +35,11 @@ public class PhoneBook {
     public void get(String name){
         if (phonebook.get(name) == null){
             System.out.println(name + " отсутсвует в списке");
-        }else {
-            System.out.println(name + " : " + phonebook.get(name));
+//        }else {
+//            System.out.println(name + " : " + phonebook.get(name));
+//            что-то подобное
+        }else if (phonebook.containsKey(name)){
+            System.out.println(phonebook.values().toString());
         }
     }
 
