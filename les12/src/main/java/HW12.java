c547cb027e8d38e6124c6e8358809a8099774dc1
kosykt <kosvasl09@gmail.com>
2021-04-03 12:10:37 +0900

    runnanble

--- les12/src/main/java/HW12.java
+++ les12/src/main/java/HW12.java
@@ -1,6 +1,22 @@
 public class HW12 {
 
+    static class MyRunnableClass implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(100);
+                    System.out.println(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+        new Thread(new MyRunnableClass()).start();
+        new Thread(new MyRunnableClass()).start();
     }
 }
+
