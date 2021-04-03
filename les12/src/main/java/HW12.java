627e1dab19568ba1b4a0852b5cd72d88df86c46c
kosykt <kosvasl09@gmail.com>
2021-04-03 21:38:08 +0900

    cleaning

--- les12/src/main/java/HW12.java
+++ les12/src/main/java/HW12.java
@@ -1,38 +1,6 @@
 public class HW12 {
 
-    private Object lock1 = new Object();
-
     public static void main(String[] args) {
-        HW12 e2 = new HW12();
-        System.out.println("Старт main потока");
-        new Thread(() -> e2.method1()).start();
-        new Thread(() -> e2.method1()).start();
-    }
-
-    public void method1() {
-        System.out.println("Метод запущен");
-        System.out.println("Блок 1 начало");
-        for (int i = 0; i < 3; i++) {
-            System.out.println(i);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("Блок 1 конец");
-        synchronized (lock1) {
-            System.out.println("Начало синхронизированного блока");
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Конец синхронизированного блока");
-        }
-        System.out.println("Метод завершил свою работу");
+        
     }
 }
