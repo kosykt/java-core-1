41743162b6f7b5f0c8fdbf2c4ed0f33b75929cd5
kosykt <kosvasl09@gmail.com>
2021-04-03 22:00:33 +0900

    counter

--- les12/src/main/java/HW12.java
+++ les12/src/main/java/HW12.java
@@ -1,6 +1,40 @@
 public class HW12 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread t1 = new Thread(new MyRunnable(counter));
+        t1.start();
+        t1.join();
+        System.out.println(counter.getCount());
+
+    }
+}
+
+class Counter{
+
+    private long count = 1;
+
+    public long getCount() {
+        return count;
+    }
+
+    public long getNextId() {
+        return count++;
+    }
+}
+
+class MyRunnable implements Runnable {
+
+    private Counter counter;
+
+    public MyRunnable(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 5000; i++) {
+            counter.getNextId();
+        }
     }
 }
