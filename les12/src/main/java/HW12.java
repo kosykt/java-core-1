cc2492021cc288f6554430028a8142fb7d284e55
kosykt <kosvasl09@gmail.com>
2021-04-03 15:05:32 +0900

    synchronized

--- les12/src/main/java/HW12.java
+++ les12/src/main/java/HW12.java
@@ -1,36 +1,38 @@
 public class HW12 {
 
-    static class MyRunnableClass implements Runnable {
-        @Override
-        public void run() {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    Thread.sleep(100);
-                    System.out.println(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    static class MyThread extends Thread {
-        @Override
-        public void run() {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    Thread.sleep(100);
-                    System.out.println(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+    private Object lock1 = new Object();
 
     public static void main(String[] args) {
-        new Thread(new MyRunnableClass()).start();
-        new MyThread().start();
+        HW12 e2 = new HW12();
+        System.out.println("Старт main потока");
+        new Thread(() -> e2.method1()).start();
+        new Thread(() -> e2.method1()).start();
+    }
+
+    public void method1() {
+        System.out.println("Метод запущен");
+        System.out.println("Блок 1 начало");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Блок 1 конец");
+        synchronized (lock1) {
+            System.out.println("Начало синхронизированного блока");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Конец синхронизированного блока");
+        }
+        System.out.println("Метод завершил свою работу");
     }
 }
-
