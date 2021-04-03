4279c662f61d0a661eef935121cbcc57868f333f
kosykt <kosvasl09@gmail.com>
2021-04-04 00:47:49 +0900

    mythread

--- les12/src/main/java/HW12/HW12.java
+++ les12/src/main/java/HW12/HW12.java
@@ -2,6 +2,21 @@ package HW12;
 
 public class HW12 {
     public static void main(String[] args) {
-        
+        Thread t1 = new Thread();
+        Thread t2 = new Thread();
+    }
+
+    public static void MyThreadOne(){
+        int SIZE = 10000000;
+        int HALF = SIZE / 2;
+        float[] arr = new float[SIZE];
+        float[] arrHalf = new float[HALF];
+        Long counter;
+    }
+
+    public static void MyThreadTwo(){
+        int SIZE = 10000000;
+        int HALF = SIZE / 2;
+        float[] arr = new float[SIZE];
     }
 }
