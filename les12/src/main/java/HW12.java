8c07bae80f99f2e189c769a9c4757e824ef53c39
kosykt <kosvasl09@gmail.com>
2021-04-03 12:12:31 +0900

    thread

--- les12/src/main/java/HW12.java
+++ les12/src/main/java/HW12.java
@@ -14,9 +14,23 @@ public class HW12 {
         }
     }
 
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(100);
+                    System.out.println(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         new Thread(new MyRunnableClass()).start();
-        new Thread(new MyRunnableClass()).start();
+        new MyThread().start();
     }
 }
 
