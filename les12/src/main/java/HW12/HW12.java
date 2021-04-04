1115ad868e19a317f505477bae33474091f787f6
kosykt <kosvasl09@gmail.com>
2021-04-05 01:01:11 +0900

    done

--- les12/src/main/java/HW12/HW12.java
+++ les12/src/main/java/HW12/HW12.java
@@ -6,6 +6,7 @@ public class HW12 {
 
     int SIZE = 10;
     int HALF = SIZE / 2;
+    float NUM = 1.0f;
 
     public static void main(String[] args) {
 
@@ -19,12 +20,12 @@ public class HW12 {
         float[] arr = new float[SIZE];
 
         for (int i = 0; i < SIZE; i++) {
-            arr[i] = 1.0f;
+            arr[i] = NUM;
         }
 
         long a = System.currentTimeMillis();
 
-        MyRunnable1 thr = new MyRunnable1(arr);
+        MyRunnable thr = new MyRunnable(arr);
 
         Thread t = new Thread(thr);
 
@@ -34,8 +35,6 @@ public class HW12 {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-//        System.out.println(Arrays.toString(thr.getArr()));
 
         System.out.println("Время первого потока(t1): " + (System.currentTimeMillis() - a));
     }
@@ -51,89 +50,41 @@ public class HW12 {
         long a = System.currentTimeMillis();
 
         for (int i = 0; i < SIZE; i++) {
-            arr[i] = 1.0f;
+            arr[i] = NUM;
         }
 
         System.arraycopy(arr, 0, arrHalf1, 0, arrHalf1.length);
         System.arraycopy(arr, arrHalf1.length, arrHalf2,0 , arrHalf2.length);
 
-//        System.out.println(Arrays.toString(arrHalf1));
-//        System.out.println(Arrays.toString(arrHalf2));
-
-
-        MyRunnable2 thr2 = new MyRunnable2(arrHalf1);
-        MyRunnable3 thr3 = new MyRunnable3(arrHalf2);
+        MyRunnable thr1 = new MyRunnable(arrHalf1);
+        MyRunnable thr2 = new MyRunnable(arrHalf2);
+        Thread t1 = new Thread(thr1);
         Thread t2 = new Thread(thr2);
-        Thread t3 = new Thread(thr3);
 
+        t1.start();
         t2.start();
-        t3.start();
         try {
+            t1.join();
             t2.join();
-            t3.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-//        System.out.println(Arrays.toString(thr2.getArr()));
-//        System.out.println(Arrays.toString(thr3.getArr()));
+        arrHalf1 = thr1.getArr();
+        arrHalf2 = thr2.getArr();
 
-//        arrHalf1 = thr2.getArr();
-//        arrHalf2 = thr3.getArr();
-//
-//        System.out.println(Arrays.toString(arrHalf1));
-
-
+        System.arraycopy(arrHalf1, 0, arr, 0, arrHalf1.length);
+        System.arraycopy(arrHalf2, 0, arr, arrHalf1.length, arrHalf2.length);
 
         System.out.println("Время второго потока(t2): " + (System.currentTimeMillis() - a));
     }
 }
 
-class MyRunnable1 implements Runnable {
+class MyRunnable implements Runnable {
 
     private float[] arr;
 
-    public MyRunnable1(float[] arr) {
-        this.arr = arr;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
-        }
-    }
-
-    public float[] getArr() {
-        return arr;
-    }
-}
-
-class MyRunnable2 implements Runnable {
-
-    private float[] arr;
-
-    public MyRunnable2(float[] arr) {
-        this.arr = arr;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
-        }
-    }
-
-    public float[] getArr() {
-        return arr;
-    }
-}
-
-class MyRunnable3 implements Runnable {
-
-    private float[] arr;
-
-    public MyRunnable3(float[] arr) {
+    public MyRunnable(float[] arr) {
         this.arr = arr;
     }
 
