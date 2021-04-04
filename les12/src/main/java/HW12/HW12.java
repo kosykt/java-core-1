10250a3b68ad5e7672086043baaae61579720c1e
kosykt <kosvasl09@gmail.com>
2021-04-04 17:13:44 +0900

    change methods

--- les12/src/main/java/HW12/HW12.java
+++ les12/src/main/java/HW12/HW12.java
@@ -4,37 +4,47 @@ import java.util.Arrays;
 
 public class HW12 {
 
-
     public static void main(String[] args) {
-        Thread t1 = new Thread(new MyThreadOne());
-//        Thread t2 = new Thread();
-    }
-}
+        HW12 wh = new HW12();
 
-class MyThreadOne implements Runnable{
-    private static final int SIZE = 10;
-    private static final int HALF = SIZE / 2;
-    private float[] arr1 = new float[SIZE];
-    private float[] arr2 = new float[HALF];
-    private int counter = 1;
-
-    public MyThreadOne() {
-        for (int i = 0; i < SIZE; i++) {
-            arr1[i] = counter;
-        }
-        run();
+        new Thread(() -> wh.MyThreadOne()).start();
+        new Thread(() -> wh.MyThreadTwo()).start();
     }
 
-
-
-    @Override
-    public void run() {
-        System.out.println(Arrays.toString(arr1));
+    public void MyThreadOne(){
+        int SIZE = 10;
+        int HALF = SIZE / 2;
+        float[] arr = new float[SIZE];
+        int num = 1;
         long a = System.currentTimeMillis();
 
-        
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = num;
+        }
 
-        System.out.println(System.currentTimeMillis() - a);
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.print("Время первого потока(t1): " + (System.currentTimeMillis() - a));
+        System.out.println();
+    }
+
+    public void MyThreadTwo(){
+        int SIZE = 10;
+        int HALF = SIZE / 2;
+        float[] arr = new float[SIZE];
+        int num = 1;
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = num;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.print("Время второго потока(t2): " + (System.currentTimeMillis() - a));
+        System.out.println();
     }
 }
 
