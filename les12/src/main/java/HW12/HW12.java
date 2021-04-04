5b99af2af3dfad5750bba5b72b261a1601bdbaa5
kosykt <kosvasl09@gmail.com>
2021-04-04 22:29:54 +0900

    clear

--- les12/src/main/java/HW12/HW12.java
+++ les12/src/main/java/HW12/HW12.java
@@ -1,18 +1,26 @@
 package HW12;
 
-import java.util.Arrays;
-
 public class HW12 {
 
+    int SIZE = 10;
+    int HALF = SIZE / 2;
+
     public static void main(String[] args) {
+
         HW12 hw = new HW12();
 
         new Thread(() -> hw.MyThreadOne()).start();
         new Thread(() -> hw.MyThreadTwo()).start();
     }
 
+    public float[] calculate(float[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
+        return arr;
+    }
+
     public void MyThreadOne() {
-        int SIZE = 10;
+
         float[] arr = new float[SIZE];
 
         for (int i = 0; i < SIZE; i++) {
@@ -20,16 +28,13 @@ public class HW12 {
         }
 
         long a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(arr));
+
         calculate(arr);
-        System.out.println(Arrays.toString(arr));
 
         System.out.println("Время первого потока(t1): " + (System.currentTimeMillis() - a));
     }
 
     public void MyThreadTwo() {
-        int SIZE = 10;
-        int HALF = SIZE / 2;
         float[] arrOriginal = new float[SIZE];
 
         float[] arrHalf1 = new float[HALF];
@@ -40,13 +45,7 @@ public class HW12 {
         }
 
         long a = System.currentTimeMillis();
-        
-//        System.out.println("Время второго потока(t2): " + (System.currentTimeMillis() - a));
-    }
 
-    public float[] calculate(float[] arr) {
-        for (int i = 0; i < arr.length; i++)
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
-        return arr;
+        System.out.println("Время второго потока(t2): " + (System.currentTimeMillis() - a));
     }
 }
