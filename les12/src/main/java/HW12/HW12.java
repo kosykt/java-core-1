8236c33520a7e5068b325a39c53396377f23b5a1
kosykt <kosvasl09@gmail.com>
2021-04-04 14:13:15 +0900

    millis

--- les12/src/main/java/HW12/HW12.java
+++ les12/src/main/java/HW12/HW12.java
@@ -1,22 +1,41 @@
 package HW12;
 
+import java.util.Arrays;
+
 public class HW12 {
+
+
     public static void main(String[] args) {
-        Thread t1 = new Thread();
-        Thread t2 = new Thread();
-    }
-
-    public static void MyThreadOne(){
-        int SIZE = 10000000;
-        int HALF = SIZE / 2;
-        float[] arr = new float[SIZE];
-        float[] arrHalf = new float[HALF];
-        Long counter;
-    }
-
-    public static void MyThreadTwo(){
-        int SIZE = 10000000;
-        int HALF = SIZE / 2;
-        float[] arr = new float[SIZE];
+        Thread t1 = new Thread(new MyThreadOne());
+//        Thread t2 = new Thread();
     }
 }
+
+class MyThreadOne implements Runnable{
+    private static final int SIZE = 10;
+    private static final int HALF = SIZE / 2;
+    private float[] arr1 = new float[SIZE];
+    private float[] arr2 = new float[HALF];
+    private int counter = 1;
+
+    public MyThreadOne() {
+        for (int i = 0; i < SIZE; i++) {
+            arr1[i] = counter;
+        }
+        run();
+    }
+
+
+
+    @Override
+    public void run() {
+        System.out.println(Arrays.toString(arr1));
+        long a = System.currentTimeMillis();
+
+        
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
+}
+
+
