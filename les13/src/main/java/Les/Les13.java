151359fdd8760f80de49b5a142d9470f7f909c71
kosykt <kosvasl09@gmail.com>
2021-04-09 13:49:54 +0900

    execute methods

--- les13/src/main/java/Les/Les13.java
+++ les13/src/main/java/Les/Les13.java
@@ -1,28 +1,68 @@
 package Les;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class Les13 {
+
+    private static int SIZE = 1000000;
+    private static int PART_SIZE = 200000;
+
     public static void main(String[] args) {
+        long[] ints = new long[SIZE];
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            ints[i] = random.nextInt();
+        }
+        long start = System.currentTimeMillis();
+        System.out.println(Arrays.stream(ints).sum());
+        System.out.println("first " + (System.currentTimeMillis() - start));
 
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
-
-        int[] a = {1,2,3,4,5,6,7,8,9};
-
-        Callable<Integer> task = () -> Arrays.stream(a).sum();
-        Future<Integer> submit = fixedThreadPool.submit(task);
-
-        submit.isDone();
-        try {
-            Integer i = submit.get();
-            System.out.println(i);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        start = System.currentTimeMillis();
+        List<Summa> summas = new ArrayList<>(SIZE / PART_SIZE);
+        for (int i = 0; i < SIZE; i += PART_SIZE) {
+            summas.add(new Summa(ints, i, Math.min(i + PART_SIZE, SIZE)));
         }
 
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(SIZE / PART_SIZE);
+        try {
+            List<Future<Long>> futures = fixedThreadPool.invokeAll(summas);
+            long sum = futures.stream()
+                    .mapToLong(f -> {
+                        try {
+                            return f.get();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).sum();
+            System.out.println(sum);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("second " + (System.currentTimeMillis() - start));
         fixedThreadPool.shutdown();
+    }
+}
+
+class Summa implements Callable<Long>{
+    long[] a;
+    int start, end;
+
+    public Summa(long[] a, int start, int end) {
+        this.a = a;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Long call() throws Exception {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += a[i];
+        }
+        return sum;
     }
 }
