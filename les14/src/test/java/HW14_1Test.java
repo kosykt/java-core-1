52529f347ac5d8add453777473babf264846f790
kosykt <kosvasl09@gmail.com>
2021-04-10 22:53:10 +0900

    testing

--- les14/src/test/java/HW14_1Test.java
+++ les14/src/test/java/HW14_1Test.java
@@ -1,10 +1,16 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HW14_1Test {
 
     public static HW14_1 hw;
+    public static int[] arr = {1, 2, 3, 4, 5};
+    public static int[] arr1 = {5};
 
     @Before
     public void init(){
@@ -17,4 +23,19 @@ public class HW14_1Test {
         int[] b = {5};
         Assert.assertArrayEquals(b, hw.checkArr(a));
     }
+
+//    @BeforeEach
+//    public void init() {
+//        hw = new HW14_1();
+//    }
+//
+//    @CsvSource({
+//            "arr, arr1"
+//    })
+//    @ParameterizedTest
+//    public void checkArr1(int[] a, int[] result){
+//        Assertions.assertArrayEquals(result, hw.checkArr(a));
+//    }
+
+
 }
